7194125eedc9a60a1dfa3b30c050cf3c07a930f0
Yuliya <>
2022-10-13 19:13:21 +0300

    done

--- 04_Text/randomize.sh
+++ 04_Text/randomize.sh
@@ -5,31 +5,32 @@ coords=()
 art=()
 t=${1:-0.01}
 
-while IFS= read -r line
+while read -r line
 do
     art+=("$line")
-    for j in $(seq 0 $((${#line}-1)) )
-    do
-        coords+=("$i $j")
-    done
+  	if [ ${line:$i:1} != " " ]
+  	then
+      for j in $(seq 0 $((${#line}-1)) )
+      do
+          coords+=("$i $j")
+      done
+    fi
     ((i++))
 done
-
-coords="$(echo -ne "$coords" | shuf)"
+coords="$(echo -n "$coords" | shuf)"
 
 tput clear
-echo "$art" |
+echo -n "$art" |
 for idx in $(shuf -i 0-$((${#coords[@]}-1)) )
 do
-    x=$(echo ${coords[$idx]} | cut -f 1 -d " ")
-    y=$(echo ${coords[$idx]} | cut -f 2 -d " ")
+    x=$(echo -n ${coords[$idx]} | cut -f 1 -d " ")
+    y=$(echo -n ${coords[$idx]} | cut -f 2 -d " ")
     tput cup $x $y
-    echo "${art[$x]:$y:1}"
+    echo -n "${art[$x]:$y:1}"
     sleep $t
 
 done
 tput cup "$(tput lines)" 0
-tr -d '\n'
-echo
+echo -n
 
 
